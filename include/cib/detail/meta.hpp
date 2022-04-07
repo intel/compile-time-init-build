7249ca4502d5b4651a3714149104582608ef5a77
Luke Valenty <luke.valenty@intel.com>
2022-04-07 10:50:25 -0700

    clean function signatures and doxygen in meta.hpp

--- include/cib/detail/meta.hpp
+++ include/cib/detail/meta.hpp
@@ -12,76 +12,126 @@ namespace cib::detail {
     template<int value>
     CIB_CONSTEXPR auto int_ = std::integral_constant<int, value>{};
 
+    /**
+     * Used by fold_right to leverage c++17 fold expressions with arbitrary
+     * callables.
+     *
+     * @tparam ValueType
+     *      The type of the element from the value pack.
+     *
+     * @tparam CallableType
+     *      A callable that takes two arguments, current element to be
+     *      processed and the fold state.
+     */
     template<
-        typename ValueT,
-        typename CallableT>
+        typename ValueType,
+        typename CallableType>
     struct fold_helper {
-        ValueT const & value;
-        CallableT const & op;
+        ValueType const & element;
+        CallableType const & operation;
 
-        CIB_CONSTEXPR fold_helper(
-            ValueT const & value,
-            CallableT const & op
+        [[nodiscard]] CIB_CONSTEXPR fold_helper(
+            ValueType const & element,
+            CallableType const & operation
         )
-            : value{value}
-            , op{op}
+            : element{element}
+            , operation{operation}
         {}
 
-        template<typename StateT>
+        template<typename StateType>
         [[nodiscard]] CIB_CONSTEXPR inline auto operator+(
-            StateT const & state
+            StateType const & state
         ) const {
-            return op(value, state);
+            return operation(element, state);
         }
     };
 
+    /**
+     * fold_right a tuple of elements.
+     *
+     * Fold operations are sometimes called accumulate or reduce in other
+     * languages or libraries.
+     *
+     * https://en.wikipedia.org/wiki/Fold_%28higher-order_function%29
+     *
+     * @param operation
+     *      A callable that takes the current element being processed
+     *      and the current state, and returns the state to be used
+     *      to process the next element. Called for each element in
+     *      the tuple.
+     *
+     * @return
+     *      The final state of all of the operations.
+     */
     template<
-        typename... T,
-        typename InitT,
-        typename CallableT>
+        typename... ElementTypes,
+        typename InitType,
+        typename CallableType>
     [[nodiscard]] CIB_CONSTEXPR inline static auto fold_right(
-        std::tuple<T...> const & t,
-        InitT const & init,
-        CallableT const & op
+        std::tuple<ElementTypes...> const & elements,
+        InitType const & initial_state,
+        CallableType const & operation
     ) {
-        return std::apply([&](auto const & ... e){
-            return (fold_helper{e, op} + ... + init);
-        }, t);
+        return std::apply([&](auto const & ... element_pack){
+            return (fold_helper{element_pack, operation} + ... + initial_state);
+        }, elements);
     }
 
+    /**
+     * Perform an operation on each element of an integral sequence.
+     *
+     * @param sequence
+     * @param operation
+     *      The operation to perform. Must be a callable that accepts a single parameter.
+     */
     template<
-        typename T,
-        T... i,
-        typename CallableT>
+        typename IntegralType,
+        IntegralType... Indices,
+        typename CallableType>
     CIB_CONSTEXPR inline static void for_each(
-        std::integer_sequence<T, i...> const &,
-        CallableT const & op
+        [[maybe_unused]] std::integer_sequence<IntegralType, Indices...> const & sequence,
+        CallableType const & operation
     ) {
-        (op(std::integral_constant<int, i>{}) , ...);
+        (operation(std::integral_constant<IntegralType, Indices>{}) , ...);
     }
 
+    /**
+     * Perform an operation on each element of an integral sequence from 0 to
+     * num_elements.
+     *
+     * @param num_elements
+     * @param operation
+     *      The operation to perform. Must be a callable that accepts a single parameter.
+     */
     template<
-        typename T,
-        T size,
-        typename CallableT>
+        typename IntegralType,
+        IntegralType NumElements,
+        typename CallableType>
     CIB_CONSTEXPR inline void for_each(
-        std::integral_constant<T, size> const &,
-        CallableT const & op
+        [[maybe_unused]] std::integral_constant<IntegralType, NumElements> const & num_elements,
+        CallableType const & operation
     ) {
-        CIB_CONSTEXPR auto seq = std::make_integer_sequence<T, size>{};
-        for_each(seq, op);
+        CIB_CONSTEXPR auto seq = std::make_integer_sequence<IntegralType, NumElements>{};
+        for_each(seq, operation);
     }
 
+    /**
+     * Perform an operation on each element of a tuple.
+     *
+     * @param num_elements
+     * @param operation
+     *      The operation to perform. Must be a callable that accepts a single parameter.
+     */
     template<
-        typename... Ts,
-        typename CallableT>
+        typename... ElementTypes,
+        typename CallableType>
     CIB_CONSTEXPR inline void for_each(
-        std::tuple<Ts...> const & tuple,
-        CallableT const & op
+        std::tuple<ElementTypes...> const & elements,
+        CallableType const & operation
     ) {
-        std::apply([&](auto const & ... pack){
-            (op(pack) , ...);
-        }, tuple);
+        std::apply([&](auto const & ... element_pack){
+            (operation(element_pack) , ...);
+        }, elements);
     }
 }
 
