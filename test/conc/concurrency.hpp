1e7874ed21d019037433edf83841607228fe348d
Ben Deane <ben.deane@intel.com>
2022-11-04 16:55:51 -0600

    :art: Inject ConcurrencyPolicy into dynamic_controller
    It's better to pass a policy as a template argument than rely on a global
    dependency.

--- test/conc/concurrency.hpp
+++ test/conc/concurrency.hpp
@@ -1,42 +1,57 @@
 #pragma once
 
-// NOTE: this is a mock implementation of concurrency
+#include <utility>
 
-namespace conc {
-template <typename Callable> inline void critical_section(Callable callable) {
-    callable();
-}
+// NOTE: this is a mock implementation of concurrency: it does nothing but
+// fulfil the required interface.
 
-/**
- * Safely poll on condition before entering a critical section.
- *
- * This construct ensures that the condition can be polled on without blocking
- * higher priority tasks or interrupts from being executed.
- *
- * @param condition Callable that returns true if the critical section can be
- * entered
- * @param callable  Callable to be executed within the critical section
- */
-template <typename ConditionType, typename CallableType>
-inline void critical_section(ConditionType condition, CallableType callable) {
-    while (true) {
-        if (condition()) {
-            // execute the main body of the critical section
-            callable();
-            return;
-        } else {
-            // if condition() is false, then re-enable interrupts to give higher
-            // priority tasks and interrupts an opportunity to be serviced
-            // before checking condition() again
+namespace test {
+
+class ConcurrencyPolicy {
+    /**
+     * An RAII object that represents holding a critical section. Execution
+     * enters the critical section on construction and leaves it on
+     * destruction.
+     */
+    class [[nodiscard]] CriticalSection {};
+
+  public:
+    /**
+     * Call a callable under a critical section.
+     *
+     * @param callable Callable to be executed within the critical section
+     */
+    template <typename CallableT>
+    static inline auto call_in_critical_section(CallableT &&callable)
+        -> decltype(auto) {
+        [[maybe_unused]] CriticalSection cs{};
+        return std::forward<CallableT>(callable)();
+    }
+
+    /**
+     * Safely poll on condition before entering a critical section.
+     *
+     * This construct ensures that the condition can be polled on without
+     * blocking higher priority tasks or interrupts from being executed.
+     *
+     * @param predicate Callable that returns true if the critical section can
+     *                  be entered
+     * @param callable  Callable to be executed within the critical section
+     */
+    template <typename PredicateT, typename CallableT>
+    static inline auto call_in_critical_section(PredicateT &&predicate,
+                                                CallableT &&callable)
+        -> decltype(auto) {
+        while (true) {
+            [[maybe_unused]] CriticalSection cs{};
+            if (predicate()) {
+                return std::forward<CallableT>(callable)();
+            }
+            // if predicate() is false, then re-enable interrupts to give
+            // higher priority tasks and interrupts an opportunity to be
+            // serviced before checking again
         }
     }
-}
-
-class CriticalSection {
-  public:
-    CriticalSection() {}
-
-    ~CriticalSection() {}
 };
 
-} // namespace conc
+} // namespace test
