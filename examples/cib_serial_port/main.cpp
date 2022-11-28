8473bf5557dc0b3a8939e0e38200179fd55ff2e0
batbug <bv.basavaraju@gmail.com>
2022-11-28 22:42:49 +0530

    + format cpp file with clang-format!

--- examples/cib_serial_port/main.cpp
+++ examples/cib_serial_port/main.cpp
@@ -1,82 +1,76 @@
-#include<cib/cib.hpp>
+#include <cib/cib.hpp>
 
-#include<iostream>
+#include <iostream>
 
-//Services - basically like a function prototypes - callback function declaration!!
-struct send_byte_t : public cib::callback_meta<uint8_t>
-{};
+// Services - basically like a function prototypes - callback function
+// declaration!!
+struct send_byte_t : public cib::callback_meta<uint8_t> {};
 
-struct get_byte_t : public cib::callback_meta<>
-{};
+struct get_byte_t : public cib::callback_meta<> {};
 
-struct serial_port_init_t : public cib::callback_meta<>
-{};
+struct serial_port_init_t : public cib::callback_meta<> {};
 
-struct run_t : public cib::callback_meta<>
-{};
+struct run_t : public cib::callback_meta<> {};
 
-//components defintion!!
-struct core_component_t
-{
-  constexpr static auto config = cib::config(
+// components defintion!!
+struct core_component_t {
+    constexpr static auto config = cib::config(
 
-    cib::exports<send_byte_t>,
-    cib::extend<send_byte_t>([](uint8_t byte){
-      std::cout << "From CoreComponent: " << byte << std::endl;
-    }),
+        cib::exports<send_byte_t>,
 
-    cib::extend<get_byte_t>([](){
-      uint8_t readByte;
-      std::cin >> readByte;
+        cib::extend<send_byte_t>([](uint8_t byte) {
+            std::cout << "From CoreComponent: " << byte << std::endl;
+        }),
 
-      cib::service<send_byte_t>(readByte); // This invokes both implementation of the SendByte - TxComponent and CoreComponent implementation
-    }),
-    
-    cib::extend<serial_port_init_t>([](){
-      std::cout<<"Initialized\n";
-    }),
+        cib::extend<get_byte_t>([]() {
+            uint8_t readByte;
+            std::cin >> readByte;
 
-    cib::extend<run_t>([](){
-      std::cout << "Type Something!!\n";
-    })
-  );
+            // This invokes both implementation of the send_byte_t
+            // from tx_component_t and core_component_t
+            cib::service<send_byte_t>(readByte);
+        }),
+
+        cib::extend<serial_port_init_t>([]() { std::cout << "Initialized\n"; }),
+
+        cib::extend<run_t>([]() { std::cout << "Type Something!!\n"; }));
 };
 
-struct tx_component_t
-{
-  //tx - feature which implement a function which takes uint8_t argument! - send_byte_t service!!
-  constexpr static auto tx = [](uint8_t byte) {
-    std::cout << "From TxComponent: " << byte << std::endl;
-  };
+struct tx_component_t {
+    // tx - feature which implement a function which takes uint8_t argument! -
+    // send_byte_t service!!
+    constexpr static auto tx = [](uint8_t byte) {
+        std::cout << "From TxComponent: " << byte << std::endl;
+    };
 
-  constexpr static auto config = cib::config(
-    cib::exports<send_byte_t>,
-    cib::extend<send_byte_t>(tx)
-    );
+    constexpr static auto config = cib::config(
+
+        cib::exports<send_byte_t>,
+
+        cib::extend<send_byte_t>(tx));
 };
 
-struct export_components_t
-{
-  constexpr static auto config = cib::exports<get_byte_t, serial_port_init_t, run_t>;
+struct export_components_t {
+    constexpr static auto config =
+        cib::exports<get_byte_t, serial_port_init_t, run_t>;
 };
 
-//Project definition with mutiple components
-struct dummy_serial_port_project_t
-{
-  constexpr static auto config = cib::components<core_component_t, tx_component_t, export_components_t>;
+// Project definition with mutiple components
+struct dummy_serial_port_project_t {
+    constexpr static auto config =
+        cib::components<core_component_t, tx_component_t, export_components_t>;
 };
 
 static cib::nexus<dummy_serial_port_project_t> nexus{};
 
-int main()
-{
-  nexus.init(); // This is critical!! otherwise accessing service from another service will result in the segmentation fault!!
-  nexus.service<serial_port_init_t>();
+int main() {
+    nexus.init(); // This is critical!! otherwise accessing service from another
+                  // service will result in the segmentation fault!!
+    nexus.service<serial_port_init_t>();
 
-  while(true)
-  {
-    nexus.service<run_t>();
-    nexus.service<get_byte_t>();
-  }
-  return 0;
+    while (true) {
+        nexus.service<run_t>();
+        nexus.service<get_byte_t>();
+    }
+    return 0;
 }
--- examples/flow_daily_routine/main.cpp
+++ examples/flow_daily_routine/main.cpp
@@ -1,10 +1,10 @@
-#include<cib/cib.hpp>
-#include<flow/flow.hpp>
+#include <cib/cib.hpp>
+#include <flow/flow.hpp>
 
-#include<chrono>
-#include<iostream>
+#include <chrono>
+#include <iostream>
 
-//In this example we shall see the Daily routine of a individual person
+// In this example we shall see the Daily routine of a individual person
 /*
   Following actions to be done in order in a day by a person
     + WakeUp
@@ -22,8 +22,10 @@
     + HaveDinner
     + GoToBed
 
-  Morning_Routine_ts are:  WakeUp, GetReadyForExercise, Exercise, TakeBath, GetReadyToWork, HaveBreakfast, GoToOffice
-  Evening_Routine_ts are:  ReturnHome, GetReadyToExercise, Exercise, TakeBath, RelaxForSometime, HaveDinner, GoToBed
+  Morning_Routine_ts are:  WakeUp, GetReadyForExercise, Exercise, TakeBath,
+  GetReadyToWork, HaveBreakfast, GoToOffice
+  Evening_Routine_ts are:  ReturnHome, GetReadyToExercise, Exercise, TakeBath,
+  RelaxForSometime, HaveDinner, GoToBed
 
   These things should be done in order!!
   Flow part of the CIB library will help us to achieve the same
@@ -33,15 +35,18 @@
     + Flow::MileStone<>
     + operator >>
     + operator &&
-  
+
   Flow::Service ==> 2 or more Actions and/or MileStones
   >>            ==> used to link actions/milestones to be executed in order
   &&            ==> used to link actions/milestones without any order!!
 
-  Note:: 
-    services can be defined using 
-      + cib::callback_meta<>  - can have only one function pointer assigned to execute
-      + flow::service<>       - set of actions/milestones which are to be executed in specific order. So basically sequential execution of multiple function pointers!!
+  Note::
+    services can be defined using
+      + cib::callback_meta<>  - can have only one function pointer assigned to
+  execute
+      + flow::service<>       - set of actions/milestones which are to be
+  executed in specific order. So basically sequential execution of multiple
+  function pointers!!
 
     both services are different.
 
@@ -55,249 +60,170 @@
 /*
   Service definitions!!
 */
-//struct morning_routine_t : public flow::service<decltype("morning_routine_t"_sc)> //decltype("morning_routine_t"_sc) - string constant "morning_routine_t" is used during the logging
-struct morning_routine_t : public flow::service<> //When logging or naming is not needed!!
-{};
+// decltype("morning_routine_log_t"_sc) - used during the logging
+// struct morning_routine_log_t
+//     : public flow::service<decltype("morning_routine_log_t"_sc)> {};
+struct morning_routine_t : public flow::service<> {};
 
-//struct evening_routine_t : public flow::service<decltype("evening_routine_t"_sc)>
-struct evening_routine_t : public flow::service<>
-{};
+// struct evening_routine_t : public
+// flow::service<decltype("evening_routine_t"_sc)>
+struct evening_routine_t : public flow::service<> {};
 
 /*
   Component definitions!!
 */
-struct self_care_component_t
-{
-  /*
-    Actions: WAKE_UP, EXERCISE, GO_TO_BED, RELAX, TAKE_BATH
-  */
-  constexpr static auto WAKE_UP = flow::action(
-    "WakeUp"_sc,
-    []()
-    {
-      std::cout<<"Wake up at 6:00 AM" <<std::endl;
-    }
-  );
+struct self_care_component_t {
+    /*
+      Actions: WAKE_UP, EXERCISE, GO_TO_BED, RELAX, TAKE_BATH
+    */
+    constexpr static auto WAKE_UP = flow::action(
+        "WakeUp"_sc, []() { std::cout << "Wake up at 6:00 AM" << std::endl; });
 
-  constexpr static auto EXERCISE = flow::action(
-    "Exercise"_sc,
-    []()
-    {
-      std::cout<<"Gym activities!!" <<std::endl;
-    }
-  );
+    constexpr static auto EXERCISE = flow::action(
+        "Exercise"_sc, []() { std::cout << "Gym activities!!" << std::endl; });
 
+    constexpr static auto GO_TO_BED = flow::action("GoToBed"_sc, []() {
+        std::cout << "Go to bed at 10:00 PM" << std::endl;
+    });
 
-  constexpr static auto GO_TO_BED = flow::action(
-    "GoToBed"_sc,
-    []()
-    {
-      std::cout<<"Go to bed at 10:00 PM" <<std::endl;
-    }
-  );
+    constexpr static auto RELAX = flow::action("Relax"_sc, []() {
+        std::cout << "Have relax before Dinner" << std::endl;
+    });
 
-  constexpr static auto RELAX = flow::action(
-    "Relax"_sc,
-    []()
-    {
-      std::cout << "Have relax before Dinner" << std::endl;
-    }
-  );
+    constexpr static auto TAKE_BATH = flow::action(
+        "TakeBath"_sc, []() { std::cout << "Take a bath" << std::endl; });
 
-  constexpr static auto TAKE_BATH = flow::action(
-    "TakeBath"_sc,
-    []()
-    {
-      std::cout << "Take a bath" << std::endl;
-    }
-  );
+    // Extend flow services!!
+    constexpr static auto config = cib::config(
 
-  //Extend flow services!!
-  constexpr static auto config = cib::config(
-    cib::extend<morning_routine_t>(
-      self_care_component_t::WAKE_UP >>
-      self_care_component_t::EXERCISE >>
-      self_care_component_t::TAKE_BATH
-    ),
+        cib::extend<morning_routine_t>(self_care_component_t::WAKE_UP >>
+                                       self_care_component_t::EXERCISE >>
+                                       self_care_component_t::TAKE_BATH),
 
-    cib::extend<evening_routine_t>(
-      self_care_component_t::EXERCISE >>
-      self_care_component_t::TAKE_BATH >>
-      self_care_component_t::RELAX >>
-      self_care_component_t::GO_TO_BED
-    )
-  );
+        cib::extend<evening_routine_t>(self_care_component_t::EXERCISE >>
+                                       self_care_component_t::TAKE_BATH >>
+                                       self_care_component_t::RELAX >>
+                                       self_care_component_t::GO_TO_BED));
 };
 
-struct food_component_t
-{
-  /*
-    Actions: BREAKFAST, DINNER
-  */
-  constexpr static auto BREAKFAST = flow::action(
-    "Breakfast"_sc,
-    []()
-    {
-      std::cout << "Have healthy breakfast" << std::endl;
-    }
-  );
+struct food_component_t {
+    /*
+      Actions: BREAKFAST, DINNER
+    */
+    constexpr static auto BREAKFAST = flow::action("Breakfast"_sc, []() {
+        std::cout << "Have healthy breakfast" << std::endl;
+    });
 
-  constexpr static auto DINNER = flow::action(
-    "Dinner"_sc,
-    []()
-    {
-      std::cout << "Have early dinner" << std::endl;
-    }
-  );
+    constexpr static auto DINNER = flow::action(
+        "Dinner"_sc, []() { std::cout << "Have early dinner" << std::endl; });
 
-  //Extend flow services!!
-  constexpr static auto config = cib::config(
-    cib::extend<morning_routine_t>(
-      self_care_component_t::TAKE_BATH >>
-      food_component_t::BREAKFAST
-    ),
+    // Extend flow services!!
+    constexpr static auto config = cib::config(
 
-    cib::extend<evening_routine_t>(
-      self_care_component_t::RELAX >>
-      food_component_t::DINNER >>
-      self_care_component_t::GO_TO_BED
-    )
-  );
+        cib::extend<morning_routine_t>(self_care_component_t::TAKE_BATH >>
+                                       food_component_t::BREAKFAST),
+
+        cib::extend<evening_routine_t>(self_care_component_t::RELAX >>
+                                       food_component_t::DINNER >>
+                                       self_care_component_t::GO_TO_BED));
 };
 
-struct dress_up_component_t
-{
-  /*
-    Actions: GET_READY_FOR_EXERCISE, GET_READY_TO_WORK
-  */
-  constexpr static auto GET_READY_FOR_EXERCISE = flow::action(
-    "GetReadyForExercise"_sc,
-    []()
-    {
-      std::cout << "Sports wear" << std::endl;
-    }
-  );
+struct dress_up_component_t {
+    /*
+      Actions: GET_READY_FOR_EXERCISE, GET_READY_TO_WORK
+    */
+    constexpr static auto GET_READY_FOR_EXERCISE =
+        flow::action("GetReadyForExercise"_sc,
+                     []() { std::cout << "Sports wear" << std::endl; });
 
-  constexpr static auto GET_READY_TO_WORK = flow::action(
-    "GetReadyToWork"_sc,
-    []()
-    {
-      std::cout << "Office wear" << std::endl;
-    }
-  );
+    constexpr static auto GET_READY_TO_WORK = flow::action(
+        "GetReadyToWork"_sc, []() { std::cout << "Office wear" << std::endl; });
 
-  //Extend flow services!!
-  constexpr static auto config = cib::config(
-    cib::extend<morning_routine_t>(
-      self_care_component_t::WAKE_UP >>
-      dress_up_component_t::GET_READY_FOR_EXERCISE >>
-      self_care_component_t::EXERCISE >>
-      self_care_component_t::TAKE_BATH >>
-      dress_up_component_t::GET_READY_TO_WORK >>
-      food_component_t::BREAKFAST
-    ),
-  
-    cib::extend<evening_routine_t>(
-      dress_up_component_t::GET_READY_FOR_EXERCISE >>
-      self_care_component_t::EXERCISE
-    )
-  );
+    // Extend flow services!!
+    constexpr static auto config = cib::config(
+
+        cib::extend<morning_routine_t>(
+            self_care_component_t::WAKE_UP >>
+            dress_up_component_t::GET_READY_FOR_EXERCISE >>
+            self_care_component_t::EXERCISE >>
+            self_care_component_t::TAKE_BATH >>
+            dress_up_component_t::GET_READY_TO_WORK >>
+            food_component_t::BREAKFAST),
+
+        cib::extend<evening_routine_t>(
+            dress_up_component_t::GET_READY_FOR_EXERCISE >>
+            self_care_component_t::EXERCISE));
 };
 
-struct commute_component_t
-{
-  /*
-    Actions: GO_TO_OFFICE, RETURN_HOME
-  */
-  constexpr static auto GO_TO_OFFICE = flow::action(
-    "GoToOffice"_sc,
-    []()
-    {
-      std::cout << "Commute to office" << std::endl;
-    }
-  );
+struct commute_component_t {
+    /*
+      Actions: GO_TO_OFFICE, RETURN_HOME
+    */
+    constexpr static auto GO_TO_OFFICE = flow::action("GoToOffice"_sc, []() {
+        std::cout << "Commute to office" << std::endl;
+    });
 
-  constexpr static auto RETURN_HOME = flow::action(
-    "ReturnHome"_sc,
-    []()
-    {
-      std::cout << "Commute to home" << std::endl;
-    }
-  );
+    constexpr static auto RETURN_HOME = flow::action(
+        "ReturnHome"_sc, []() { std::cout << "Commute to home" << std::endl; });
 
-  //Extend flow services!!
-  constexpr static auto config = cib::config(
-    cib::extend<morning_routine_t>(
-      food_component_t::BREAKFAST  >>
-      commute_component_t::GO_TO_OFFICE
-    ),
-    cib::extend<evening_routine_t>(
-      commute_component_t::RETURN_HOME >>
-      dress_up_component_t::GET_READY_FOR_EXERCISE
-    )
-  );
+    // Extend flow services!!
+    constexpr static auto config = cib::config(
+
+        cib::extend<morning_routine_t>(food_component_t::BREAKFAST >>
+                                       commute_component_t::GO_TO_OFFICE),
+
+        cib::extend<evening_routine_t>(
+            commute_component_t::RETURN_HOME >>
+            dress_up_component_t::GET_READY_FOR_EXERCISE));
 };
 
-struct daily_routine_component_t
-{
-  constexpr static auto DAILY_ROUTINES = flow::action(
-    "Daily Routines"_sc,
-    []()
-    {
-      static const auto ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
+struct daily_routine_component_t {
+    constexpr static auto DAILY_ROUTINES =
+        flow::action("Daily Routines"_sc, []() {
+            static const auto ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
-      static bool daysRoutineComplete = false;
-      static uint32_t dayCount = 1;
-      static auto startTime = std::chrono::system_clock::now();
-      
+            static bool daysRoutineComplete = false;
+            static uint32_t dayCount = 1;
+            static auto startTime = std::chrono::system_clock::now();
 
-      if(!daysRoutineComplete)
-      {
-        std::cout << "----- Day: " << dayCount << " -----\n";
-        flow::run<morning_routine_t>();
-        flow::run<evening_routine_t>();
-        daysRoutineComplete = true;
-      }
+            if (!daysRoutineComplete) {
+                std::cout << "----- Day: " << dayCount << " -----\n";
+                flow::run<morning_routine_t>();
+                flow::run<evening_routine_t>();
+                daysRoutineComplete = true;
+            }
 
-      auto currentTime = std::chrono::system_clock::now();
-      auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime);
-      if(elapsedTime.count() >= ONE_DAY_IN_MILLISECONDS)
-      {
-        dayCount++;
-        startTime = currentTime;
-        daysRoutineComplete = false;
-      }
-    }
-  );
+            auto currentTime = std::chrono::system_clock::now();
+            auto elapsedTime =
+                std::chrono::duration_cast<std::chrono::milliseconds>(
+                    currentTime - startTime);
+            if (elapsedTime.count() >= ONE_DAY_IN_MILLISECONDS) {
+                dayCount++;
+                startTime = currentTime;
+                daysRoutineComplete = false;
+            }
+        });
 
-  constexpr static auto config = cib::config(
-    cib::exports<
-      morning_routine_t,
-      evening_routine_t>,    
+    constexpr static auto config = cib::config(
 
-    // we need to extend the MainLoop as cib::top implements MainLoop service
-    cib::extend<cib::MainLoop>(
-      DAILY_ROUTINES
-    )
-  );
+        cib::exports<morning_routine_t, evening_routine_t>,
+
+        // we need to extend the MainLoop as cib::top implements
+        // MainLoop service
+        cib::extend<cib::MainLoop>(DAILY_ROUTINES));
 };
 
-struct person_routine_proj
-{
-  constexpr static auto config = cib::components<
-      daily_routine_component_t,
-      self_care_component_t,
-      food_component_t,
-      dress_up_component_t,
-      commute_component_t
-    >;
+struct person_routine_proj {
+    constexpr static auto config =
+        cib::components<daily_routine_component_t, self_care_component_t,
+                        food_component_t, dress_up_component_t,
+                        commute_component_t>;
 };
 
 cib::top<person_routine_proj> top{};
 
-int main()
-{
-  top.main();
+int main() {
+    top.main();
 
-  return 0;
+    return 0;
 }
