a72ab9c26fe9b201ae8e45e08db17bdda6cb9805
Ben Deane <ben@elbeno.com>
2023-02-17 14:53:23 -0700

    :fire: Remove unused set functions

--- include/cib/set.hpp
+++ include/cib/set.hpp
@@ -1,41 +1,14 @@
 #pragma once
 
 #include <cib/detail/set_details.hpp>
+#include <cib/tuple.hpp>
+
+#include <array>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
 
 namespace cib {
-template <typename LhsTuple, typename RhsTuple, typename MetaFunc = self_type>
-[[nodiscard]] constexpr auto set_union(LhsTuple lhs, RhsTuple rhs,
-                                       MetaFunc meta_func = self_type{}) {
-    return set_operation_impl(
-        detail::set_operation_algorithm<true, true, true>{}, meta_func, lhs,
-        rhs);
-}
-
-template <typename LhsTuple, typename RhsTuple, typename MetaFunc = self_type>
-[[nodiscard]] constexpr auto
-set_intersection(LhsTuple lhs, RhsTuple rhs, MetaFunc meta_func = self_type{}) {
-    return set_operation_impl(
-        detail::set_operation_algorithm<false, true, false>{}, meta_func, lhs,
-        rhs);
-}
-
-template <typename LhsTuple, typename RhsTuple, typename MetaFunc = self_type>
-[[nodiscard]] constexpr auto set_difference(LhsTuple lhs, RhsTuple rhs,
-                                            MetaFunc meta_func = self_type{}) {
-    return set_operation_impl(
-        detail::set_operation_algorithm<true, false, false>{}, meta_func, lhs,
-        rhs);
-}
-
-template <typename LhsTuple, typename RhsTuple, typename MetaFunc = self_type>
-[[nodiscard]] constexpr auto
-set_symmetric_difference(LhsTuple lhs, RhsTuple rhs,
-                         MetaFunc meta_func = self_type{}) {
-    return set_operation_impl(
-        detail::set_operation_algorithm<true, false, true>{}, meta_func, lhs,
-        rhs);
-}
-
 template <auto... Indexes, typename Operation>
 [[nodiscard]] constexpr auto make_tuple_from_op(std::index_sequence<Indexes...>,
                                                 Operation op) {
