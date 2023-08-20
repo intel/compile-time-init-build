94746acef6ab7715cafbf0879fb2b4d7fa6c0099
Ben Deane <ben@elbeno.com>
2023-08-19 19:40:51 -0600

    :art: Simplify `builder_meta` machinery
    - simplify exposing `builder_t` and `interface_t`
    - remove vestigial `traits` namespace

--- include/cib/detail/extend.hpp
+++ include/cib/detail/extend.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cib/detail/builder_traits.hpp>
+#include <cib/builder_meta.hpp>
 #include <cib/detail/compiler.hpp>
 #include <cib/detail/config_item.hpp>
 #include <cib/tuple.hpp>
@@ -9,7 +9,7 @@ namespace cib::detail {
 template <typename ServiceType, typename... Args>
 struct extend : public config_item {
     using service_type = ServiceType;
-    constexpr static auto builder = cib::traits::builder_v<service_type>;
+    constexpr static auto builder = cib::builder_t<service_type>{};
     cib::tuple<Args...> args_tuple;
 
     CIB_CONSTEVAL explicit extend(Args const &...args) : args_tuple{args...} {}
--- test/cib/builder_meta.cpp
+++ test/cib/builder_meta.cpp
@@ -1,19 +1,20 @@
-#include <cib/cib.hpp>
+#include <cib/builder_meta.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 
 #include <type_traits>
 
-struct TestBuilderTag {};
-struct TestInterfaceTag {};
+namespace {
+struct TestBuilderTag;
+struct TestInterfaceTag;
 
 struct test_builder_meta
     : public cib::builder_meta<TestBuilderTag, TestInterfaceTag> {};
+} // namespace
 
 TEST_CASE(
     "builder_meta builder and interface type traits return correct values") {
-    REQUIRE(std::is_same_v<TestBuilderTag,
-                           cib::traits::builder_t<test_builder_meta>>);
-    REQUIRE(std::is_same_v<TestInterfaceTag,
-                           cib::traits::interface_t<test_builder_meta>>);
+    REQUIRE(std::is_same_v<TestBuilderTag, cib::builder_t<test_builder_meta>>);
+    REQUIRE(
+        std::is_same_v<TestInterfaceTag, cib::interface_t<test_builder_meta>>);
 }
