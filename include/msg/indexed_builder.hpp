dcb25f33bda135c7ac52e99f77e72623c5fddcbd
Ben Deane <ben.deane@intel.com>
2023-08-23 15:00:00 -0600

    :art: Fixups for new tuple functionality and container changes

--- include/msg/indexed_builder.hpp
+++ include/msg/indexed_builder.hpp
@@ -2,50 +2,49 @@
 
 #include <cib/builder_meta.hpp>
 #include <cib/tuple.hpp>
-#include <container/Vector.hpp>
+#include <container/constexpr_map.hpp>
+#include <container/vector.hpp>
 #include <log/log.hpp>
-#include <msg/indexed_handler.hpp>
-#include <msg/message.hpp>
-#include <msg/match.hpp>
-#include <msg/field_matchers.hpp>
 #include <lookup/lookup.hpp>
 #include <msg/detail/bitset.hpp>
-
-#include <container/ConstexprMap.hpp>
+#include <msg/field_matchers.hpp>
+#include <msg/indexed_handler.hpp>
+#include <msg/match.hpp>
+#include <msg/message.hpp>
 
 #include <array>
 #include <cstddef>
 #include <type_traits>
 
 namespace msg {
+template <typename T> using get_field_type = typename T::field_type;
+
 namespace detail {
-template<typename BuilderValue>
+template <typename BuilderValue>
 consteval auto for_each_callback(auto fn) -> void {
     constexpr auto t = BuilderValue::value.callbacks;
-    [&]<std::size_t... i>(std::index_sequence<i...>) -> void {
-        (fn(t.get(cib::index_<i>), i), ...);
-    }(std::make_index_sequence<t.size()>{});
+    [&]<std::size_t... i>(std::index_sequence<i...>)->void {
+        (fn(t[cib::index<i>], i), ...);
+    }
+    (std::make_index_sequence<t.size()>{});
 }
-
-struct get_field_type {
-    template <typename T>
-    using invoke = typename T::field_type;
-};
 
 consteval auto with_field_index(auto t) {
-    return cib::transform<get_field_type>([](auto x, auto){return x;}, t, t);
+    return cib::transform<get_field_type>([](auto x, auto) { return x; }, t, t);
 }
-}
+} // namespace detail
 
-
-template<typename FieldType, std::size_t EntryCapacity, std::size_t CallbackCapacity>
+template <typename FieldType, std::size_t EntryCapacity,
+          std::size_t CallbackCapacity>
 struct temp_index {
     using field_type = FieldType;
 
-    ConstexprMap<uint32_t, detail::bitset<CallbackCapacity>, EntryCapacity> entries{};
+    cib::constexpr_map<uint32_t, detail::bitset<CallbackCapacity>,
+                       EntryCapacity>
+        entries{};
     detail::bitset<CallbackCapacity> default_value{};
 
-    constexpr auto & operator[](auto key) {
+    constexpr auto &operator[](auto key) {
         if (!entries.contains(key)) {
             entries.put(key, {});
         }
@@ -54,40 +53,28 @@ struct temp_index {
     }
 };
 
-struct get_field_type {
-    template <typename T>
-    using invoke = typename T::field_type;
-};
-
-struct get_identity {
-    template <typename T>
-    using invoke = T;
-};
-
-
 // TODO: needs index configuration
 template <typename IndexSpec, typename CallbacksT, typename BaseMsgT,
           typename... ExtraCallbackArgsT>
 struct indexed_builder {
     CallbacksT callbacks;
 
-    template <typename T> [[nodiscard]] consteval auto add(T callback) {
-        auto new_callbacks =
-            cib::tuple_cat(callbacks, cib::make_tuple(callback));
-
+    template <typename... Ts> [[nodiscard]] constexpr auto add(Ts... ts) {
+        auto new_callbacks = cib::tuple_cat(callbacks, cib::make_tuple(ts...));
         using new_callbacks_t = decltype(new_callbacks);
-
         return indexed_builder<IndexSpec, new_callbacks_t, BaseMsgT,
                                ExtraCallbackArgsT...>{new_callbacks};
     }
 
     template <typename FieldType, typename T, T... ExpectedValues>
-    static consteval auto get_matchers(in_t<FieldType, T, ExpectedValues...> m) {
+    static consteval auto
+    get_matchers(in_t<FieldType, T, ExpectedValues...> m) {
         return cib::make_tuple(m);
     }
 
     template <typename FieldType, typename T, T ExpectedValue>
-    static consteval auto get_matchers(equal_to_t<FieldType, T, ExpectedValue> m) {
+    static consteval auto
+    get_matchers(equal_to_t<FieldType, T, ExpectedValue> m) {
         return cib::make_tuple(m);
     }
 
@@ -96,102 +83,105 @@ struct indexed_builder {
         return m.matchers;
     }
 
-    using callback_func_t = void(*)(BaseMsgT const &, ExtraCallbackArgsT... args);
+    using callback_func_t = void (*)(BaseMsgT const &,
+                                     ExtraCallbackArgsT... args);
 
-    template<typename BuilderValue, std::size_t... i>
-    static consteval auto create_callback_array(
-        std::index_sequence<i...>
-    ) -> std::array<callback_func_t, BuilderValue::value.callbacks.size()> {
-        return {
-            // FIXME: incomplete message callback invocation...
-            //        1) bit_cast message argument
-            //        2) log message match
-            //        3) unindexed fields checking
-            BuilderValue::value.callbacks.get(cib::index_<i>).callable...
-        };
+    template <typename BuilderValue, std::size_t... i>
+    static consteval auto create_callback_array(std::index_sequence<i...>)
+        -> std::array<callback_func_t, BuilderValue::value.callbacks.size()> {
+        return {// FIXME: incomplete message callback invocation...
+                //        1) bit_cast message argument
+                //        2) log message match
+                //        3) unindexed fields checking
+                BuilderValue::value.callbacks[cib::index<i>].callable...};
     }
 
-    template<typename BuilderValue>
+    template <typename BuilderValue>
     static consteval auto create_temp_indices() {
         IndexSpec indices{};
-        detail::for_each_callback<BuilderValue>([&](auto callback, auto callback_num){
+        detail::for_each_callback<BuilderValue>([&](auto callback,
+                                                    auto callback_num) {
             // FIXME: need to convert matcher to product of sums
             auto const matchers =
                 detail::with_field_index(get_matchers(callback.matcher));
 
-            indices.for_each([&]<typename T>(T) -> void {
-                using field_type = typename T::field_type;
+            for_each(
+                [&]<typename T>(T) -> void {
+                    using field_type = typename T::field_type;
 
-                // if this callback specifies a constraint on an indexed field...
-                if constexpr (requires {matchers.get(cib::tag_<field_type>);}) {
-                    // ...then add that constraint to the index
-                    auto const field = matchers.get(cib::tag_<field_type>);
-                    field.expected_values.for_each([&](auto field_value) -> void {
-                        indices.get(cib::tag_<field_type>)[field_value].add(callback_num);
-                    });
-                } else {
-                    // ...otherwise add this callback to the index's default value
-                    indices.get(cib::tag_<field_type>).default_value.add(callback_num);
-                }
-            });
+                    // if this callback specifies a constraint on an indexed
+                    // field...
+                    if constexpr (cib::contains_type<decltype(matchers),
+                                                     field_type>) {
+                        // ...then add that constraint to the index
+                        auto const field = matchers.get(cib::tag<field_type>);
+                        for_each(
+                            [&](auto field_value) -> void {
+                                indices.get(cib::tag<field_type>)[field_value]
+                                    .add(callback_num);
+                            },
+                            field.expected_values);
+                    } else {
+                        // ...otherwise add this callback to the index's default
+                        // value
+                        indices.get(cib::tag<field_type>)
+                            .default_value.add(callback_num);
+                    }
+                },
+                indices);
         });
         return indices;
     }
 
-    template<typename BuilderValue, typename I, auto entry_num>
+    template <typename BuilderValue, typename I, auto entry_num>
     static consteval auto make_entry() {
-        constexpr IndexSpec temp_indices =
-            create_temp_indices<BuilderValue>();
+        constexpr IndexSpec temp_indices = create_temp_indices<BuilderValue>();
 
         return lookup::entry{
-            std::next(temp_indices.get(cib::tag_<I>).entries.begin(), entry_num)->key,
-            std::next(temp_indices.get(cib::tag_<I>).entries.begin(), entry_num)->value |
-            temp_indices.get(cib::tag_<I>).default_value
-        };
+            std::next(temp_indices.get(cib::tag<I>).entries.begin(), entry_num)
+                ->key,
+            std::next(temp_indices.get(cib::tag<I>).entries.begin(), entry_num)
+                    ->value |
+                temp_indices.get(cib::tag<I>).default_value};
     }
 
     template <typename BuilderValue> static consteval auto build() {
         // build callback array
-        constexpr auto num_callbacks =
-            BuilderValue::value.callbacks.size();
+        constexpr auto num_callbacks = BuilderValue::value.callbacks.size();
 
         constexpr std::array<callback_func_t, num_callbacks> callback_array =
-            create_callback_array<BuilderValue>(std::make_index_sequence<num_callbacks>{});
+            create_callback_array<BuilderValue>(
+                std::make_index_sequence<num_callbacks>{});
 
         // build temporary indices for each field
-        constexpr IndexSpec temp_indices =
-            create_temp_indices<BuilderValue>();
+        constexpr IndexSpec temp_indices = create_temp_indices<BuilderValue>();
 
+        auto const make_index_lookup = [&]<typename I,
+                                           std::size_t... entry_num>(
+                                           cib::tag_constant<I> *,
+                                           std::index_sequence<entry_num...>) {
+            return lookup::make<lookup::input<
+                uint32_t, decltype(temp_indices.get(cib::tag<I>).default_value),
+                temp_indices.get(cib::tag<I>).default_value,
+                make_entry<BuilderValue, I, entry_num>()...>>();
+        };
 
-        const auto make_index_lookup =
-            [&]<typename I, std::size_t... entry_num>(cib::tag_t<I>, std::index_sequence<entry_num...>){
-                return lookup::make<
-                    lookup::input<uint32_t, decltype(temp_indices.get(cib::tag_<I>).default_value), temp_indices.get(cib::tag_<I>).default_value,
-                        make_entry<BuilderValue, I, entry_num>()...
-                    >
-                >();
-            };
-
-        const auto entry_index_seq =
-            [&]<typename I>(cib::tag_t<I>){
-                return std::make_index_sequence<temp_indices.get(cib::tag_<I>).entries.getSize()>{};
-            };
+        auto const entry_index_seq = [&]<typename I>(cib::tag_constant<I> *) {
+            return std::make_index_sequence<
+                temp_indices.get(cib::tag<I>).entries.size()>{};
+        };
 
         constexpr auto baked_indices =
-            temp_indices.apply([&]<typename... I>(I...){
+            temp_indices.apply([&]<typename... I>(I...) {
                 return indices{
-                    index{
-                        typename I::field_type{},
-                        make_index_lookup(cib::tag_<I>, entry_index_seq(cib::tag_<I>))
-                    }...
-                };
+                    index{typename I::field_type{},
+                          make_index_lookup(cib::tag<I>,
+                                            entry_index_seq(cib::tag<I>))}...};
             });
 
         return indexed_handler{
-            callback_args_t<BaseMsgT, ExtraCallbackArgsT...>{},
-            baked_indices,
-            callback_array
-        };
+            callback_args_t<BaseMsgT, ExtraCallbackArgsT...>{}, baked_indices,
+            callback_array};
     }
 };
 
--- include/msg/indexed_handler.hpp
+++ include/msg/indexed_handler.hpp
@@ -2,7 +2,7 @@
 
 #include <cib/builder_meta.hpp>
 #include <cib/tuple.hpp>
-#include <container/Vector.hpp>
+#include <container/vector.hpp>
 #include <log/log.hpp>
 #include <msg/callback.hpp>
 #include <msg/handler_interface.hpp>
@@ -13,63 +13,54 @@
 
 namespace msg {
 
-template<typename Field, typename Lookup>
-struct index {
+template <typename Field, typename Lookup> struct index {
     Lookup field_lookup;
 
-    consteval index(Field, Lookup field_lookup_arg) : field_lookup{field_lookup_arg} {}
+    consteval index(Field, Lookup field_lookup_arg)
+        : field_lookup{field_lookup_arg} {}
 
-    constexpr auto operator()(auto const & data) const {
+    constexpr auto operator()(auto const &data) const {
         return field_lookup[Field::extract(data)];
     }
 };
 
-template<typename... IndicesT>
-struct indices : IndicesT... {
+template <typename... IndicesT> struct indices : IndicesT... {
     consteval indices(IndicesT... index_args) : IndicesT{index_args}... {}
 
-    constexpr auto operator()(auto const & data) const {
+    constexpr auto operator()(auto const &data) const {
         return (this->IndicesT::operator()(data) & ...);
     }
 };
 
-template<typename BaseMsgT, typename... ExtraCallbackArgsT>
+template <typename BaseMsgT, typename... ExtraCallbackArgsT>
 struct callback_args_t {};
 
-
-template<typename BaseMsgT, typename... ExtraCallbackArgsT>
+template <typename BaseMsgT, typename... ExtraCallbackArgsT>
 constexpr callback_args_t<BaseMsgT, ExtraCallbackArgsT...> callback_args{};
 
-template<
-    typename IndexT,
-    typename CallbacksT,
-    typename BaseMsgT,
-    typename... ExtraCallbackArgsT>
+template <typename IndexT, typename CallbacksT, typename BaseMsgT,
+          typename... ExtraCallbackArgsT>
 struct indexed_handler : handler_interface<BaseMsgT, ExtraCallbackArgsT...> {
-    using callback_func_t = void(*)(BaseMsgT const &, ExtraCallbackArgsT... args);
+    using callback_func_t = void (*)(BaseMsgT const &,
+                                     ExtraCallbackArgsT... args);
 
     IndexT index;
     CallbacksT callback_entries;
 
     constexpr explicit indexed_handler(
-        callback_args_t<BaseMsgT, ExtraCallbackArgsT...>,
-        IndexT new_index,
-        CallbacksT new_callbacks
-    )
-        : index{new_index}
-        , callback_entries{new_callbacks}
-    {}
+        callback_args_t<BaseMsgT, ExtraCallbackArgsT...>, IndexT new_index,
+        CallbacksT new_callbacks)
+        : index{new_index}, callback_entries{new_callbacks} {}
 
-    auto is_match(BaseMsgT const & msg) const -> bool final {
-        return !index(msg.data).empty();
+    auto is_match(BaseMsgT const &msg) const -> bool final {
+        return !index(msg).empty();
     }
 
     void handle(BaseMsgT const &msg, ExtraCallbackArgsT... args) const final {
-        auto const callback_candidates = index(msg.data);
+        auto const callback_candidates = index(msg);
 
-        callback_candidates.for_each([&](auto i){
-            callback_entries[i](msg, args...);
-        });
+        callback_candidates.for_each(
+            [&](auto i) { callback_entries[i](msg, args...); });
 
         if (callback_candidates.empty()) {
             CIB_ERROR("None of the registered callbacks claimed this message.");
