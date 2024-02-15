f49d7f2609e6a84cad2f6eaef478820818dbda39
Ben Deane <ben@elbeno.com>
2024-02-14 21:07:33 -0700

    :bug: Support OBJECT libraries with gen_str_catalog

---
+++ test/log/catalog2a_lib.cpp
@@ -0,0 +1,25 @@
+#include "catalog_concurrency.hpp"
+#include "catalog_enums.hpp"
+
+#include <conc/concurrency.hpp>
+#include <log/catalog/mipi_encoder.hpp>
+
+#include <cstdint>
+
+template <> inline auto conc::injected_policy<> = test_conc_policy{};
+
+extern int log_calls;
+
+namespace {
+struct test_log_args_destination {
+    auto log_by_args(std::uint32_t, auto...) -> void { ++log_calls; }
+};
+} // namespace
+
+auto log_two_rt_args() -> void;
+
+auto log_two_rt_args() -> void {
+    auto cfg = logging::mipi::config{test_log_args_destination{}};
+    cfg.logger.log_msg<logging::level::TRACE, cib_log_module_id_t>(
+        format("D string with {} and {} placeholder"_sc, 1, 2));
+}
