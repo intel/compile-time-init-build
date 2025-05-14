383a55b754d75ae90adb53bc3445a5d68a773a22
Ben Deane <ben@elbeno.com>
2025-05-13 21:47:55 -0600

    :art: Refactor string catalog generation
    Problem:
    - Ad-hoc data structures in `gen_str_catalog.py` are unwieldy.
    - Enum lookup was broken by encoding change with `encode_32` etc.
    
    Solution:
    - Introduction python classes for `Message` and `Module`; simplify code.
    - Add some tests for `gen_str_catalog.py`.
    - Fix enum lookup: include enum info inside `encode_32<T>`.

--- include/log/catalog/mipi_builder.hpp
+++ include/log/catalog/mipi_builder.hpp
@@ -16,38 +16,32 @@
 
 namespace logging::mipi {
 template <typename T>
-concept signed_packable =
-    std::signed_integral<T> and sizeof(T) <= sizeof(std::int64_t);
+concept signed_packable = std::signed_integral<stdx::underlying_type_t<T>> and
+                          sizeof(T) <= sizeof(std::int64_t);
 
 template <typename T>
 concept unsigned_packable =
-    std::unsigned_integral<T> and sizeof(T) <= sizeof(std::int64_t);
+    std::unsigned_integral<stdx::underlying_type_t<T>> and
+    sizeof(T) <= sizeof(std::int64_t);
 
 template <typename T>
-concept enum_packable = std::is_enum_v<T> and sizeof(T) <= sizeof(std::int32_t);
-
-template <typename T>
-concept packable =
-    signed_packable<T> or unsigned_packable<T> or enum_packable<T>;
+concept packable = signed_packable<T> or unsigned_packable<T>;
 
 template <typename T> struct encoding;
 
 template <signed_packable T> struct encoding<T> {
     using encode_t = stdx::conditional_t<sizeof(T) <= sizeof(std::int32_t),
-                                         encode_32, encode_64>;
+                                         encode_32<T>, encode_64<T>>;
     using pack_t = stdx::conditional_t<sizeof(T) <= sizeof(std::int32_t),
                                        std::int32_t, std::int64_t>;
 };
 
 template <unsigned_packable T> struct encoding<T> {
     using encode_t = stdx::conditional_t<sizeof(T) <= sizeof(std::int32_t),
-                                         encode_u32, encode_u64>;
+                                         encode_u32<T>, encode_u64<T>>;
     using pack_t = stdx::conditional_t<sizeof(T) <= sizeof(std::uint32_t),
                                        std::uint32_t, std::uint64_t>;
 };
-
-template <enum_packable T>
-struct encoding<T> : encoding<stdx::underlying_type_t<T>> {};
 
 template <packable T> using pack_as_t = typename encoding<T>::pack_t;
 template <packable T> using encode_as_t = typename encoding<T>::encode_t;
