d7ad69536dd78ee39d589724012cc455e220a0c6
Ben Deane <ben@elbeno.com>
2025-11-17 16:21:44 -0700

    :sparkles: Allow string ID ranges to be reserved on the command line
    Problem:
    - When generating a string catalog, it can be useful to reserve string IDs
      cheaply without having to incur the overhead of keeping "stable strings"
      around.
    
    Solution:
    - Allow reserved string ID ranges to be passed on the command line to
      `gen_str_catalog.py`.

--- cmake/string_catalog.cmake
+++ cmake/string_catalog.cmake
@@ -11,6 +11,7 @@ function(gen_str_catalog)
         GUID_ID
         GUID_MASK
         MODULE_ID_MAX
+        RESERVED_IDS
         OUTPUTS_TARGET)
     set(multiValueArgs INPUT_JSON INPUT_LIBS INPUT_HEADERS STABLE_JSON)
     cmake_parse_arguments(SC "${options}" "${oneValueArgs}" "${multiValueArgs}"
@@ -68,6 +69,9 @@ function(gen_str_catalog)
     if(SC_MODULE_ID_MAX)
         set(MODULE_ID_MAX_ARG --module_id_max ${SC_MODULE_ID_MAX})
     endif()
+    if(SC_RESERVED_IDS)
+        set(RESERVED_IDS_ARG --reserved_ids ${SC_RESERVED_IDS})
+    endif()
     if(NOT SC_GEN_STR_CATALOG)
         set(SC_GEN_STR_CATALOG ${GEN_STR_CATALOG})
     endif()
@@ -80,7 +84,7 @@ function(gen_str_catalog)
             --cpp_output ${SC_OUTPUT_CPP} --json_output ${SC_OUTPUT_JSON}
             --xml_output ${SC_OUTPUT_XML} --stable_json ${STABLE_JSON}
             ${FORGET_ARG} ${CLIENT_NAME_ARG} ${VERSION_ARG} ${GUID_ID_ARG}
-            ${GUID_MASK_ARG} ${MODULE_ID_MAX_ARG}
+            ${GUID_MASK_ARG} ${MODULE_ID_MAX_ARG} ${RESERVED_IDS_ARG}
         DEPENDS ${UNDEFS} ${INPUT_JSON} ${SC_GEN_STR_CATALOG} ${STABLE_JSON}
         COMMAND_EXPAND_LISTS)
 
